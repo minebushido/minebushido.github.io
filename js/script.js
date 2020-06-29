1e7f983d6ec3ee0d2939865fdd5a69e33fa330e7
minebushido <minebushido@gmail.com>
2020-06-29 22:28:13 +1000

    Finish copy function

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ function copyLink() {
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
+    let changeText = "Copied";
+    let originalText = "Click to copy!"
+    $('#copy-js').tooltip('dispose');
+    $('#copy-js').attr('data-original-title', changeText);
+    $('#copy-js').tooltip('show');
+    $('#copy-js').attr('data-original-title', originalText);
 }
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
