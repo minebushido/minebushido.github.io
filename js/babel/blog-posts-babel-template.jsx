12d5e0503785e1918b4c94b48b43a47f0a3eed13
minebushido <minebushido@gmail.com>
2020-07-01 21:03:20 +1000

    Work on react component

--- js/babel/blog-posts-babel-template.jsx
+++ js/babel/blog-posts-babel-template.jsx
@@ -5,62 +5,49 @@ const e = React.createElement;
 class LikeButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            liked: false
-        };
+        this.state = { liked: false };
     }
 
     render() {
-        return e("div", {
-            className: "card"
-        }, React.createElement("h5", {
-            className: "card-header"
-        }, "Welcome to MineBushido!"), React.createElement("div", {
-            className: "card-body"
-        }, React.createElement("p", {
-            className: "subtitle-date"
-        }, "Posted on: "), React.createElement("p", {
-            className: "card-text"
-        }, "First Post"), React.createElement("div", {
-            className: "modal-btn-container"
-        }, React.createElement("button", {
-            type: "button",
-            className: "btn btn-primary read-more-btn",
-            "data-toggle": "modal",
-            "data-target": "#exampleModal"
-        }, "READ MORE")), React.createElement("div", {
-            className: "modal fade",
-            id: "exampleModal",
-            tabindex: "-1",
-            role: "dialog",
-            "aria-labelledby": "exampleModalLabel",
-            "aria-hidden": "true"
-        }, React.createElement("div", {
-            className: "modal-dialog",
-            role: "document"
-        }, React.createElement("div", {
-            className: "modal-content"
-        }, React.createElement("div", {
-            className: "modal-header"
-        }, React.createElement("h5", {
-            className: "modal-title",
-            id: "exampleModalLabel"
-        }, "First Post"), React.createElement("button", {
-            type: "button",
-            className: "close",
-            "data-dismiss": "modal",
-            "aria-label": "Close"
-        }, React.createElement("span", {
-            "aria-hidden": "true"
-        }, "\xD7"))), React.createElement("div", {
-            className: "modal-body"
-        }, "..."), React.createElement("div", {
-            className: "modal-footer"
-        }, React.createElement("button", {
-            type: "button",
-            className: "btn btn-secondary",
-            "data-dismiss": "modal"
-        }, "Close")))))));
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+        return e(
+            <div className="card">
+                <h5 className="card-header">Welcome to MineBushido!</h5>
+                <div className="card-body">
+                    <p className="subtitle-date">Posted on: </p>
+                    <p className="card-text">First Post</p>
+                    <div className="modal-btn-container">
+                        <button type="button" className="btn btn-primary read-more-btn" data-toggle="modal"
+                            data-target="#exampleModal">
+                            READ MORE
+                                    </button>
+                    </div>
+                    <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">First Post</h5>
+                                    <button type="button" className="close" data-dismiss="modal"
+                                        aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    ...
+                                            </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary"
+                                        data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
--- js/blog-posts.js
+++ js/blog-posts.js
@@ -100,7 +100,17 @@ var LikeButton =
 
             _this = _possibleConstructorReturn(this, _getPrototypeOf(LikeButton).call(this, props));
             _this.state = {
-                liked: false
+                blogPosts: [{
+                    id: 2,
+                    title: 'second post',
+                    date: '29/06/2020',
+                    content: 'test'
+                }, {
+                    id: 1,
+                    title: 'Welcome to MineBushido!',
+                    date: '29/06/2020',
+                    content: 'test'
+                }]
             };
             return _this;
         }
@@ -108,56 +118,65 @@ var LikeButton =
         _createClass(LikeButton, [{
             key: "render",
             value: function render() {
-                return e("div", {
-                    className: "card"
-                }, React.createElement("h5", {
-                    className: "card-header"
-                }, "Welcome to MineBushido!"), React.createElement("div", {
-                    className: "card-body"
-                }, React.createElement("p", {
-                    className: "subtitle-date"
-                }, "Posted on: "), React.createElement("p", {
-                    className: "card-text"
-                }, "First Post"), React.createElement("div", {
-                    className: "modal-btn-container"
-                }, React.createElement("button", {
-                    type: "button",
-                    className: "btn btn-primary read-more-btn",
-                    "data-toggle": "modal",
-                    "data-target": "#exampleModal"
-                }, "READ MORE")), React.createElement("div", {
-                    className: "modal fade",
-                    id: "exampleModal",
-                    tabindex: "-1",
-                    role: "dialog",
-                    "aria-labelledby": "exampleModalLabel",
-                    "aria-hidden": "true"
-                }, React.createElement("div", {
-                    className: "modal-dialog",
-                    role: "document"
-                }, React.createElement("div", {
-                    className: "modal-content"
-                }, React.createElement("div", {
-                    className: "modal-header"
-                }, React.createElement("h5", {
-                    className: "modal-title",
-                    id: "exampleModalLabel"
-                }, "First Post"), React.createElement("button", {
-                    type: "button",
-                    className: "close",
-                    "data-dismiss": "modal",
-                    "aria-label": "Close"
-                }, React.createElement("span", {
-                    "aria-hidden": "true"
-                }, "\xD7"))), React.createElement("div", {
-                    className: "modal-body"
-                }, "..."), React.createElement("div", {
-                    className: "modal-footer"
-                }, React.createElement("button", {
-                    type: "button",
-                    className: "btn btn-secondary",
-                    "data-dismiss": "modal"
-                }, "Close")))))));
+                var testing = function (title, date, content) {
+                    return (e("div", {
+                        className: "card"
+                    }, React.createElement("h5", {
+                        className: "card-header"
+                    }, title), React.createElement("div", {
+                        className: "card-body"
+                    }, React.createElement("p", {
+                        className: "subtitle-date"
+                    }, "Posted on: ", date), React.createElement("p", {
+                        className: "card-text"
+                    }, content), React.createElement("div", {
+                        className: "modal-btn-container"
+                    }, React.createElement("button", {
+                        type: "button",
+                        className: "btn btn-primary read-more-btn",
+                        "data-toggle": "modal",
+                        "data-target": "#exampleModal"
+                    }, "READ MORE")), React.createElement("div", {
+                        className: "modal fade",
+                        id: "exampleModal",
+                        tabindex: "-1",
+                        role: "dialog",
+                        "aria-labelledby": "exampleModalLabel",
+                        "aria-hidden": "true"
+                    }, React.createElement("div", {
+                        className: "modal-dialog",
+                        role: "document"
+                    }, React.createElement("div", {
+                        className: "modal-content"
+                    }, React.createElement("div", {
+                        className: "modal-header"
+                    }, React.createElement("h5", {
+                        className: "modal-title",
+                        id: "exampleModalLabel"
+                    }, "First Post"), React.createElement("button", {
+                        type: "button",
+                        className: "close",
+                        "data-dismiss": "modal",
+                        "aria-label": "Close"
+                    }, React.createElement("span", {
+                        "aria-hidden": "true"
+                    }, "\xD7"))), React.createElement("div", {
+                        className: "modal-body"
+                    }, "..."), React.createElement("div", {
+                        className: "modal-footer"
+                    }, React.createElement("button", {
+                        type: "button",
+                        className: "btn btn-secondary",
+                        "data-dismiss": "modal"
+                    }, "Close"))))))))
+                }
+
+                var output = [];
+                for (var i = 0; i < this.state.blogPosts.length; i++) {
+                    output.push(testing(this.state.blogPosts[i].title, this.state.blogPosts[i].date, this.state.blogPosts[i].content));
+                }
+
+                return output
             }
         }]);
 
